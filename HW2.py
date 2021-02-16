009287985cca598ff2eecd6fbded86f2236dedca
hamzaayric <74771620+hamzaayric@users.noreply.github.com>
2021-02-16 22:18:56 +0300

    Update HW2.py

--- HW2.py
+++ HW2.py
@@ -1,14 +1,14 @@
 SatırSayısı = 3
 SütunSayısı = 3
 
-
+print("\n Rastgele 9 tane asal sayı girin \n")
 matrix = []
 
 
 for i in range(SatırSayısı):
     a = []
     for j in range(SütunSayısı):
-        AsalSayı = int(input("Asal sayı giriniz : "))
+        AsalSayı = int(input("Asal sayı : "))
         a.append(AsalSayı)
     matrix.append(a)
 
